ad54f5e51098fcbc15b44cde100eb9c535421fb2
Fayven <fayven.amelga@gmail.com>
2022-12-22 20:13:43 -0500

    Add while loop

--- script.js
+++ script.js
@@ -9,10 +9,37 @@ var maxLenth = 128;
 // var num = true;
 // var specCh = true;
 
-var lChChoices = ["a", "b", "c", "d", "e"]
-
-// Add console.log typeof
-console.log(typeof createPw);
+var lChChoices = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f'",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var spCh =
+  // Add console.log typeof
+  console.log(typeof createPw);
 console.log(typeof minLength);
 console.log(typeof maxLenth);
 // console.log(typeof lowerCase);
@@ -21,69 +48,72 @@ console.log(typeof maxLenth);
 // console.log(typeof specCh);
 
 // Add expressions and logical and operations
-console.log(minLength <= maxLenth)
-console.log(maxLenth >= minLength)
+console.log(minLength <= maxLenth);
+console.log(maxLenth >= minLength);
 
-var expression1 = (minLength <= maxLenth);
-var expression2 = (maxLenth >= minLength);
-
+var expression1 = minLength <= maxLenth;
+var expression2 = maxLenth >= minLength;
 
 console.log(expression1 && expression2);
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
-function writePassword(e) {
-  // Add prompt to var length
-  var length = prompt("Enter password length. Must be between 8-128 characters in length.");
-  if (length >= minLength && length <= maxLenth) {
-    lowLet;
-  } else if(length < minLength || length > maxLenth) {
-    alert("Password must be between "+ minLength + " and "+ maxLenth + " characters");
-    return length;
-  } 
+function writePassword() {
+  var validPasswordLength = false;
+  // Add var for prompts and confirms
+  while (validPasswordLength == false) {
+    var length = prompt(
+      "Enter password length. Must be between 8-128 characters in length."
+    );
+    if (length >= minLength && length <= maxLenth) {
+      validPasswordLength = true;
+    } else {
+      alert(
+        "Password must be between " +
+          minLength +
+          " and " +
+          maxLenth +
+          " characters"
+      );
+    }
+  }
+
   var lowLet = confirm("Include lowercase letters");
+ 
 
-  generateBtn = confirm("Include uppercase letters");
-  generateBtn = confirm("Include numbers");
-  generateBtn = confirm("Include special character");
-  generateBtn = confirm("Generate password")
+  var upLet = confirm("Include uppercase letters");
+  
 
+  var num = confirm("Include numbers");
+  
 
+  spCh = confirm("Include special character");
+  
+
+  var gnPw = confirm("Generate password");
+  
 
   // function characterType() {
   //   generateBtn = confirm("Include lowercase letters?")
   // }
-//   if (minLength < 8) {
+  //   if (minLength < 8) {
 
-//     console.log("Password must be at least 8 characters.");
-//     return "Password must be at least 8 characters."
-// }
-//   if (maxLenth > 128) {
-//     console.log("Password may not exceed 128 characters.");
-// }
-  
-  var password = generatePassword();
+  //     console.log("Password must be at least 8 characters.");
+  //     return "Password must be at least 8 characters."
+  // }
+  //   if (maxLenth > 128) {
+  //     console.log("Password may not exceed 128 characters.");
+  // }
+  else var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-function generatePassword() {
-
-  if (minLength < 8) {
-    console.log("Password must be at least 8 characters.");
-}
-
-if (maxLenth > 128) {
-    console.log("Password may not exceed 128 characters.");
-}
-
-}
+function generatePassword() {}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
+generateBtn.addEventListener("click", writePassword);
