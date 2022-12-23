2e1e6aefcead6e73d7a2b9fa42f6b29913022606
Fayven <fayven.amelga@gmail.com>
2022-12-22 22:29:05 -0500

    while/if statements added to writePassword function

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 // Assignment code here
 
 // Add var for each series of prompts
-var critMet = true;
 var minLength = 8;
 var maxLenth = 128;
-// var lowerCase = true;
-// var upperCase = true;
-// var num = true;
-// var specCh = true;
 
 // Add arrays of criteria
 var lChArr = ["a", "b", "c", "d", "e", "f'", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -15,19 +10,7 @@ String(lChArr).toUpperCase;
 var intArr = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var spChArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", ":", ";", "+"];
 
-// Add console.log typeof
-console.log(typeof createPw);
-console.log(typeof minLength);
-console.log(typeof maxLenth);
-// console.log(typeof lowerCase);
-// console.log(typeof upperCase);
-// console.log(typeof num);
-// console.log(typeof specCh);
-
 // Add expressions and logical and operations
-console.log(minLength <= maxLenth);
-console.log(maxLenth >= minLength);
-
 var expression1 = minLength <= maxLenth;
 var expression2 = maxLenth >= minLength;
 
@@ -39,54 +22,30 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 function writePassword() {
-  var validPasswordLength = false;
-  // Add var for prompts and confirms
-  while (validPasswordLength == false) {
-    var length = prompt("Enter password length. Must be between 8-128 characters in length.");
-    if (length >= minLength && length <= maxLenth) {
-      validPasswordLength = true;
-    } else {
-      alert("Password must be between " + minLength +  " and " +  maxLenth + " characters");
-    }
-  }
-
-  var lowLet = confirm("Include lowercase letters");
-  if(lowLet = true) {
-    lChArr.includes()
-  } else {
-
-  }
-
+  var chCritMet = false;
+  // If one of the character var is true, the chCritMet criteria is true
+  while(chCritMet == false) {
+    // Passing string through the function confirm which returns a boolean value that is stored in its assigned var
+    var lowLet = confirm("Include lowercase letters");
+ 
+    // Passing string through the function confirm which returns a boolean value that is stored in its assigned var
   var upLet = confirm("Include uppercase letters");
-  if(upLet = true) {
-String(lChArr).includes()
-  } else {
 
-  }
-
+    // Passing string through the function confirm which returns a boolean value that is stored in its assigned var
   var num = confirm("Include numbers");
-  if(num = true) {
 
-  } else {
-
-  }
-
+    // Passing string through the function confirm which returns a boolean value that is stored in its assigned var
   var spCh = confirm("Include special character");
-  if(spCh = true) {
-  spChArr.includes()
+
+
+  if(lowLet == true || upLet == true || num == true || spCh == true) {
+    chCritMet = true;
   } else {
-
+    alert("At least one character criteria must be selected.")
   }
+}
 
-  // var critMet = confirm("Generate password");
-  // do {
-  //   generatePassword();
-  // } while(i = length) {
-  // if(lowLet = true) || (upLet = true) || (num = true) || (spCh = true);
-    
-  // } 
 
-  // }
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -95,13 +54,7 @@ String(lChArr).includes()
 }
 
 function generatePassword() {
-  do {
-    
-  ;
-  } while(Array.length = length) {
-  if(lowLet = true) || (upLet = true) || (num = true) || (spCh = true);
-    
-  } 
+
 
   }
 
@@ -111,6 +64,20 @@ function generatePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Gather password criteria
+function gatherCrit() {
+  // Create boolean variable
+  var validPasswordLength = false;
+  //Repeating loop till criteria is met
+  while (validPasswordLength == false) {
+    // Passing a string through the function prompt which returns a value that we save to length 
+    var length = prompt("Enter password length. Must be between 8-128 characters in length."); 
+    if (length >= minLength && length <= maxLenth) {
+      validPasswordLength = true;
+    } else {
+      alert("Password must be between " + minLength +  " and " +  maxLenth + " characters");
+    }
+  }
+}
 // Idenitfy desired criteria 
 // and makes sure at least one criteria is selected; while loop, change condition to exit
 // Pull desired criteria from array(s) 
