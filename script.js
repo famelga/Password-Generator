2727179a8508d10625f1050ee8ee8555b74efa87
Fayven <fayven.amelga@gmail.com>
2022-12-22 20:51:45 -0500

    Add arrays

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var maxLenth = 128;
 // var num = true;
 // var specCh = true;
 
+// Add arrays of criteria
 var lChArr = ["a", "b", "c", "d", "e", "f'", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 String(lChArr).toUpperCase;
 var intArr = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
